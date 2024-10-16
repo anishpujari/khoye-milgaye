c34baf7348bf624a0cd211d281a43635c28a2ae0
rajat12826 <pariharrajat078@gmail.com>
2024-10-17 00:53:18 +0530

    started backend

---
+++ Backend/models/User.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Name is required"],
+    },
+    email: {
+        type: String,
+        required: [true, "Email is required"],
+        unique : true,
+        validate : validator.isEmail,
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required"],
+        minlength : [6, "Password Must Be Atleast 6 characters"],
+    },
+   
+},{timestamps:true});
+const User = mongoose.model("User", userSchema);
+
+export {User}; 
