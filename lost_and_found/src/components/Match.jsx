be363664d614c8a24a91c972a05e307e14e1e922
rajat12826 <pariharrajat078@gmail.com>
2024-10-18 09:25:29 +0530

    check match.jsx

--- lost_and_found/src/components/Match.jsx
+++ lost_and_found/src/components/Match.jsx
@@ -6,7 +6,7 @@ function Match() {
    <>
    <div>
 
-
+{/* jab item.jsx pe click krke idhar aoge toh uska lostitem ka data as a prop comapre krna with one jo backend dega match api se vo category wise divided phle se hi agya */}
    </div>
    </>
   )
